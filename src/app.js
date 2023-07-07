11fa07310aae025bc7f517d0466ee73cdf59c74d
Naomi <naomistrall@hotmail.com>
2023-07-07 03:03:57 -0300

    Código atualizado para deploy no Render

--- src/app.js
+++ src/app.js
@@ -8,5 +8,5 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`API rodando na porta ${PORT}.`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`API rodando na porta ${port}.`));
--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URL);
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 try {
   await mongoClient.connect();
